378fb9acbf336a4fd39da9fff4695228c4427518
edvansabino <edvansabino@gmail.com>
2022-12-23 20:37:22 -0300

    insert subtotal text and value

--- aula-ao-vivo/aula-23-12-22/script.js
+++ aula-ao-vivo/aula-23-12-22/script.js
@@ -1,26 +1,35 @@
-let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
-let valorSubtotal = document.getElementById("valor-subtotal");
+const quantidadeSubtotal = document.getElementById("quantidade-subtotal");
+const valorSubtotal = document.getElementById("valor-subtotal");
 const addProduct = document.getElementById("btn-adicionar-produto-01");
 const quantProduct = document.getElementById("quantidade-produto-01");
 const subtractProduct = document.getElementById("btn-subtrair-produto-01");
-console.log(addProduct);
-console.log(quantProduct);
 
 let subtotalInfo = {
-  quantidade: 1,
   valor: 11.66,
 };
-
-quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
-valorSubtotal.innerText = subtotalInfo.valor;
 
 addProduct.addEventListener("click", adicionarProduto);
 subtractProduct.addEventListener("click", retirarProduto);
 
+function calcTotalValue(quantProduct) {
+  valorSubtotal.innerText = (subtotalInfo.valor * quantProduct).toFixed(2);
+}
+
+function updateSubTotalText() {
+  quantidadeSubtotal.innerText = quantProduct.value + " itens";
+  calcTotalValue(quantProduct.value);
+}
+
 function adicionarProduto() {
-  quantProduct.value = Number(quantProduct.value) + 1;
+  if (quantProduct.value < 10) {
+    quantProduct.value = Number(quantProduct.value) + 1;
+  }
+  updateSubTotalText();
 }
 
 function retirarProduto() {
-  quantProduct.value = Number(quantProduct.value) - 1;
+  if (quantProduct.value > 1) {
+    quantProduct.value = Number(quantProduct.value) - 1;
+  }
+  updateSubTotalText();
 }
