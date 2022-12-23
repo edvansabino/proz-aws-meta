b4555ac2f2017fd64f79e24bbc537bf43f71780b
edvansabino <edvansabino@gmail.com>
2022-12-23 19:49:46 -0300

    primeiro exercicio dia 23-12-22

--- aula-ao-vivo/aula-23-12-22/script.js
+++ aula-ao-vivo/aula-23-12-22/script.js
@@ -1,5 +1,10 @@
 let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
 let valorSubtotal = document.getElementById("valor-subtotal");
+const addProduct = document.getElementById("btn-adicionar-produto-01");
+const quantProduct = document.getElementById("quantidade-produto-01");
+const subtractProduct = document.getElementById("btn-subtrair-produto-01");
+console.log(addProduct);
+console.log(quantProduct);
 
 let subtotalInfo = {
   quantidade: 1,
@@ -8,3 +13,14 @@ let subtotalInfo = {
 
 quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
 valorSubtotal.innerText = subtotalInfo.valor;
+
+addProduct.addEventListener("click", adicionarProduto);
+subtractProduct.addEventListener("click", retirarProduto);
+
+function adicionarProduto() {
+  quantProduct.value = Number(quantProduct.value) + 1;
+}
+
+function retirarProduto() {
+  quantProduct.value = Number(quantProduct.value) - 1;
+}
